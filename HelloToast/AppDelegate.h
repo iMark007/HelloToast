90b4482f474ca6956958855d4ed161e43e842c6b
iMark007 <1871226362@qq.com>
2013-08-12 10:06:21 +0800

    Initial Commit

---
+++ HelloToast/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloToast
+//
+//  Created by APPXY_DEV_056 on 13-8-12.
+//  Copyright (c) 2013年 APPXY_DEV_056. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloToast/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloToast
+//
+//  Created by APPXY_DEV_056 on 13-8-12.
+//  Copyright (c) 2013年 APPXY_DEV_056. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
